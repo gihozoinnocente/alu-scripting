be237d7b253c9912f29146b41aecf7b09627d892
gihozoinnocente <i.gihozo@alustudent.com>
2023-11-07 17:33:40 +0000

    how many subscribers from reddit api

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}  # Replace with a meaningful user agent
+
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0  # Invalid subreddit or other issue
+    except:
+        return 0  # Error or exception occurred
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subscribers = number_of_subscribers(sys.argv[1])
+        print(subscribers)
