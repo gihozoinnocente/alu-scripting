39a2a66b01ba34fbc4fc7456506a5af194a4f404
gihozoinnocente <i.gihozo@alustudent.com>
2023-11-07 17:40:17 +0000

    Module documentation

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""
+This module uses the 'requests' library to retrieve the number of subscribers for a given subreddit using the Reddit API.
+
+The 'number_of_subscribers' function takes a subreddit name as input and queries the Reddit API to fetch the subscriber count.
+If the subreddit is valid, it returns the number of subscribers; otherwise, it returns 0.
+
+The 'requests' library is used for making HTTP GET requests to the Reddit API.
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}  # Replace with a meaningful user agent
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    }
 
     try:
         response = requests.get(url, headers=headers)
@@ -12,9 +23,9 @@ def number_of_subscribers(subreddit):
         if 'data' in data and 'subscribers' in data['data']:
             return data['data']['subscribers']
         else:
-            return 0  # Invalid subreddit or other issue
+            return 0  
     except:
-        return 0  # Error or exception occurred
+        return 0  
 
 if __name__ == '__main__':
     import sys
