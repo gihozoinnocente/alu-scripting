7d3ed27ded20754f901666c19e3195cb3f5913cb
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-11-12 21:57:17 +0200

    Update 3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -14,7 +14,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "python:my_app:1.0 (by /u/gihozoi)"
+        "User-Agent": "my_app:1.0 (by /u/gihozoi)"
     }
     params = {
         "after": after,
