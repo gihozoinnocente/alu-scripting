4e38f55a9256474285fda822c954469a2a6356ad
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-11-12 08:37:41 +0200

    Update 2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,11 +1,45 @@
 #!/usr/bin/python3
+"""
+2-recurse.py
+
+This module defines a recursive function 'recurse' that queries the Reddit API
+and returns a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit, the function returns None.
+
+Requirements:
+- Prototype: def recurse(subreddit, hot_list=None, after=None)
+- If not a valid subreddit, return None.
+- Invalid subreddits may return a redirect to search results. Ensure that you are not following redirects.
+- Your code will NOT pass if you are using a loop and not recursively calling the function! This /can/ be done with a loop
+  but the point is to use a recursive function. :)
+
+Usage:
+1. Call the script from the command line with a subreddit as an argument.
+
+Example:
+$ python3 2-main.py programming
+
+Author: Your Name
+"""
+
 import requests
 
 def recurse(subreddit, hot_list=None, after=None):
+    """
+    Recursively queries the Reddit API to retrieve the titles of all hot articles for a given subreddit.
+
+    Parameters:
+    - subreddit (str): The name of the subreddit to search.
+    - hot_list (list, optional): A list to store the titles of hot articles. Defaults to None.
+    - after (str, optional): The 'after' parameter for pagination. Defaults to None.
+
+    Returns:
+    - list or None: A list containing the titles of hot articles or None if the subreddit is invalid.
+    """
     if hot_list is None:
         hot_list = []
 
-    headers = {'User-Agent': 'MyBot/1.0 (by /u/gihozoi)'}  # Add your Reddit username here
+    headers = {'User-Agent': 'MyBot/1.0 (by /u/gihozoi)'}  
 
     # Base URL for the Reddit API
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
@@ -54,4 +88,3 @@ if __name__ == '__main__':
             print(len(result))
         else:
             print("None")
-
